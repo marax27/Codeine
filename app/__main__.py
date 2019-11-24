2393033991cef260c771a07c6a1c50d4cedbce9f
Kacper Tonia <kacpert314@gmail.com>
2019-11-24 11:52:09 +0100

    Remove changes from main

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,4 @@
 from .shared.configuration import Configuration
-from .computing.facade import TaskFactory, TaskPool
 
 
 def main():
@@ -7,9 +6,6 @@ def main():
         .add_json_file('config.json')
 
     print(f'Project Codeine started with configuration: {config}')
-
-    task_pool = TaskPool()
-    task_factory = TaskFactory()
 
 
 if __name__ == "__main__":
