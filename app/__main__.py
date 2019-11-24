354f05c6f6da0aecc88f453c9fde1b2084b28f27
Kacper Tonia <kacpert314@gmail.com>
2019-11-24 11:31:51 +0100

    Create necessary files. Remove interfaces.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,5 @@
 from .shared.configuration import Configuration
+from .computing.facade import TaskFactory, TaskPool
 
 
 def main():
@@ -6,6 +7,9 @@ def main():
         .add_json_file('config.json')
 
     print(f'Project Codeine started with configuration: {config}')
+
+    task_pool = TaskPool()
+    task_factory = TaskFactory()
 
 
 if __name__ == "__main__":
