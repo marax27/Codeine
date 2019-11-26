31052317e07f2460ca634148faede726aaf3cc2c
Kacper Tonia <kacpert314@gmail.com>
2019-11-26 18:53:19 +0100

    Make logger local

--- app/__main__.py
+++ app/__main__.py
@@ -3,16 +3,14 @@ from .shared.logs import get_logger, initialize
 from .computing.facade import get_computational_problem
 
 
-logger = get_logger(__package__)
-
-
 def main():
     config = Configuration(__package__) \
         .add_json_file('config.json')
 
-    computation_problem = get_computational_problem()
+    logger = get_logger(__package__)
 
-    logger.info(f'Project Codeine started with configuration: {config}')
+    computation_problem = get_computational_problem()
+    logger.info(f'Project Codeine started with configuration: {config}.')
     logger.info(f'Computational problem: {computation_problem}')
 
 
