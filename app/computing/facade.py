66a4ac109890321a2caea04e32868e37a3b4d965
Kacper Tonia <kacpert314@gmail.com>
2019-11-26 21:45:54 +0100

    Load computation problem module dynamically

--- app/computing/facade.py
+++ app/computing/facade.py
@@ -1,5 +1,12 @@
-from . import problem
+from app.shared.configuration import Configuration
+from .base import ComputationalProblem
 
 
-def get_computational_problem():
-    return problem.ComputationalProblem()
+def get_computational_problem() -> ComputationalProblem:
+    configuration = Configuration(__package__) \
+        .add_json_file('config.json')
+
+    path = 'app.computing.' + configuration.get('problemModule')
+    problem_module = __import__(path, fromlist=[None])
+
+    return problem_module.ComputationalProblem()
