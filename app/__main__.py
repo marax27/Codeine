84c3035525a1e76ca88ebb8e2451f44188b1f18e
Kacper Tonia <kacpert314@gmail.com>
2019-11-30 21:26:56 +0100

    Run broker. Handle exceptions main thread gracefully

--- app/__main__.py
+++ app/__main__.py
@@ -1,17 +1,34 @@
+from time import sleep
+from .shared.networking import ConnectionSettings
 from .shared.configuration import Configuration
 from .shared.logs import get_logger, initialize
-from .computing.facade import get_computational_problem
+from .messaging.message_broker import MessageBroker
 
 
 def main():
     config = Configuration(__package__) \
         .add_json_file('config.json')
-
+    connection_settings = config.get('Connection').bind_as(ConnectionSettings)
     logger = get_logger(__package__)
 
-    computation_problem = get_computational_problem()
-    logger.info(f'Project Codeine started with configuration: {config}.')
-    logger.info(f'Computational problem: {computation_problem}')
+    logger.info('Codeine started.')
+    broker = MessageBroker(connection_settings)
+    broker.start()
+
+    try:
+        while True:
+            for message in broker.get_messages():
+                logger.info(f'Received message: {message}')
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    except BaseException as exc:
+        logger.error(f'An unexpected exception has occurred: {exc}')
+
+    logger.info('Stopping Codeine...')
+    broker.stop()
+    broker.join()
+    logger.info('Stopped.')
 
 
 if __name__ == '__main__':
