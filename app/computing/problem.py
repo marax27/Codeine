dffdde2e41f0bda579b6563326e74eb5afc68bc8
prznoc <przemyslaw.nocon19@gmail.com>
2019-12-01 17:52:57 +0100

    problem implementation v 0.3

--- app/computing/problem.py
+++ app/computing/problem.py
@@ -1,5 +1,6 @@
 import string
 import itertools
+import hashlib
 from dataclasses import dataclass
 from typing import Optional, Set
 from . import base
@@ -7,7 +8,7 @@ from . import base
 
 @dataclass(frozen=True)
 class State(base.State):
-    password: str
+    password: bytes
 
 
 @dataclass(frozen=True)
@@ -22,21 +23,42 @@ class TaskResult(base.TaskResult):
 
 class TaskPool(base.TaskPool):
     def _create_initial_pool(self) -> Set[TaskResult]:
-        prefixes = itertools.product(string.ascii_lowercase, repeat=3)
+        prefixes = itertools.product(string.ascii_lowercase, repeat=2)
         return set(map(TaskIdentifier, map(''.join, prefixes)))
 
 
 class Task(base.Task):
     def run(self):
-        raise NotImplementedError()
-
-
+        a = 0
+        b = 0
+        c = 0
+        d = 0
+        for i in range(26**4):
+            if (a == 26):
+                a = 0
+                b += 1
+            if (b == 26):
+                b = 0
+                c += 1
+            if (c == 26):
+                c = 0
+                d = +1
+            word = self.identifier.value + chr(a+97) + chr(b+97) + chr(c+97) + chr(d+97)
+            a = a+1
+            word_byte = word.encode('utf-8')
+            hs = hashlib.sha1()
+            hs.update(word_byte)
+            hs = hs.digest()
+            if (hs == self.state.password):
+                self.result = TaskResult(word)
+        
+        
 class ComputationalProblem(base.ComputationalProblem):
     def create_task_pool(self) -> TaskPool:
         return TaskPool()
 
     def create_state(self) -> State:
-        return State('SamplePassword')
+        return State(b'(\xb75K[\xff\xcbqN\xb0v9\xd5@W\r\xe6\xf2+\xf0')
 
     def create_task(self, identifier: TaskIdentifier, state: State) -> Task:
         return Task(identifier, state)
