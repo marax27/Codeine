02492742702b29596a1ae8d4c0b7ea07aa5d5f1c
Kacper Tonia <kacpert314@gmail.com>
2019-12-02 22:26:07 +0100

    Test NETTOPO contents

--- app/tests/test_message_broker.py
+++ app/tests/test_message_broker.py
@@ -34,6 +34,10 @@ def wait_for_response():
     sleep(0.1)
 
 
+def get_imalive_packet(address: ConnectionSettings) -> Packet:
+    return Packet(b'IMALIVE{}', address)
+
+
 @pytest.fixture()
 def connection():
     result = NetworkConnectionMock()
@@ -57,7 +61,7 @@ def test_imalive_receiveImalive_sendNettopo(
         mapper: MessageMapper
         ):
     given_address = ConnectionSettingsFactory.sample()
-    given_packet = Packet(b'IMALIVE{}', given_address)
+    given_packet = get_imalive_packet(given_address)
     connection.to_receive = [given_packet]
 
     wait_for_response()
@@ -73,7 +77,7 @@ def test_nettopo_receiveImalive_nettopoDoesntContainOurAddress(
         mapper: MessageMapper
         ):
     given_address = ConnectionSettingsFactory.sample()
-    given_packet = Packet(b'IMALIVE{}', given_address)
+    given_packet = get_imalive_packet(given_address)
     connection.to_receive = [given_packet]
 
     wait_for_response()
@@ -81,3 +85,28 @@ def test_nettopo_receiveImalive_nettopoDoesntContainOurAddress(
     sent_packet: Packet = connection.sent_by_broker[0]
     message: NetTopologyMessage = mapper.map_from_bytes(sent_packet.data)
     assert given_address not in message.agents
+
+
+def test_nettopo_nettopoWithAgentsThenImalive_brokerSendsPreviouslyAcquiredAgents(
+        connection: NetworkConnectionMock,
+        mapper: MessageMapper
+        ):
+    given_addresses = [
+        ConnectionSettings('1.2.3.4', 8000),
+        ConnectionSettings('1.2.3.5', 8000),
+        ConnectionSettings('1.2.3.6', 8000)
+    ]
+    sender_address = ConnectionSettings('1.2.3.1', 23)
+    given_message = NetTopologyMessage(given_addresses)
+    message_as_bytes = mapper.map_to_bytes(given_message)
+    given_packet = Packet(message_as_bytes, sender_address)
+
+    connection.to_receive.append(given_packet)
+    wait_for_response()
+
+    connection.to_receive.append(get_imalive_packet(sender_address))
+    wait_for_response()
+
+    sent_packet: Packet = connection.sent_by_broker[0]
+    message: NetTopologyMessage = mapper.map_from_bytes(sent_packet.data)
+    assert set(given_addresses) == set(message.agents)
