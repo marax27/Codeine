a7e85c92f7ff8adf1e05d3cc68d773274dd2969a
Kacper Tonia <kacpert314@gmail.com>
2019-12-05 11:06:19 +0100

    Add CommandHandler. Reorganize Payload

--- app/messaging/broker.py
+++ app/messaging/broker.py
@@ -1,18 +1,11 @@
 from time import sleep
 from queue import Queue
 from typing import Iterable, Optional
-from dataclasses import dataclass
 from app.shared.multithreading import StoppableThread
 from app.shared.networking import Packet, ConnectionSettings, NetworkConnection
 from .commands import CommandMapper, Command
 from .topology import Topology, NetworkCommand, ImAliveCommand, NetTopologyCommand
-
-
-@dataclass(frozen=True)
-class Payload:
-    '''Command + information where to send it.'''
-    command: Command
-    recipient_id: Optional[int]
+from .command_handler import CommandHandler, CommandNotRegisteredException, Payload
 
 
 class Broker(StoppableThread):
@@ -23,16 +16,17 @@ class Broker(StoppableThread):
         self._send_queue = Queue()
         self._recv_queue = Queue()
         self._command_mapper = self._create_command_mapper()
+        self._command_handler = self._create_command_handler()
 
     def get_commands(self) -> Iterable[Command]:
         while not self._recv_queue.empty():
             yield self._recv_queue.get()
 
     def broadcast(self, command: Command):
-        self.send_to(command, None)
+        self.send(Payload(command, None))
 
-    def send_to(self, command: Command, recipient_id: int):
-        self._send_queue.put(Payload(command, recipient_id))
+    def send(self, payload: Payload):
+        self._send_queue.put(payload)
 
     def run(self):
         while not self.requested_stop():
@@ -43,33 +37,33 @@ class Broker(StoppableThread):
     def _handle_incoming_packet(self):
         packet = self._connection.receive()
         if packet is None:
-            pass
+            return
 
         try:
-            command = self._to_command(packet.data)
+            payload = self._to_payload(packet)
         except Exception:
             return
 
-        sender_address = packet.address
-        self._topology.add_or_update(sender_address)
+        self._topology.add_or_update(packet.address)
 
-        if isinstance(command, NetworkCommand):
-            command: NetworkCommand
-            responses = list(command.invoke(self._topology))
-            if responses:
-                response_destination = command.response_destination
-                sender_id = hash(sender_address)
-                ident = response_destination.get_recipient_id(sender_id)
-                for response in responses:
-                    self.send_to(response, ident)
+        try:
+            responses = self._command_handler.handle(payload)
+            for response in responses:
+                self.send(response)
+        except CommandNotRegisteredException:
+            self._recv_queue.put(payload)
 
     def _handle_outgoing_commands(self):
         while not self._send_queue.empty():
             payload: Payload = self._send_queue.get()
-            command, recipient_id = payload.command, payload.recipient_id
+            command, recipient_id = payload.command, payload.address_id
 
             recipients = self._get_recipients_by_id(recipient_id)
             self._send(recipients, command)
+
+    def _to_payload(self, packet: Packet) -> Payload:
+        address_id = hash(packet.address)
+        return Payload(self._to_command(packet.data), address_id)
 
     def _to_command(self, data: bytes) -> Command:
         return self._command_mapper.map_from_bytes(data)
@@ -89,3 +83,7 @@ class Broker(StoppableThread):
         return CommandMapper() \
             .register(ImAliveCommand) \
             .register(NetTopologyCommand)
+
+    def _create_command_handler(self) -> CommandHandler:
+        return CommandHandler() \
+            .register(NetworkCommand, self._topology)
---
+++ app/messaging/command_handler.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+from typing import Any, Dict, Iterable, Optional, Tuple
+from dataclasses import dataclass
+from .commands import Command
+
+
+@dataclass(frozen=True)
+class Payload:
+    '''Command + information where to send it.'''
+    command: Command
+    address_id: Optional[int]
+
+
+class CommandHandler:
+    def __init__(self):
+        self._registered_types: Dict[type, Any] = dict()
+
+    def register(self, command_type: type, receiver: Any) -> CommandHandler:
+        if receiver is None:
+            raise NoneReceiverException(command_type)
+        self._registered_types[command_type] = receiver
+        return self
+
+    def handle(self, payload: Payload) -> Iterable[Payload]:
+        command, address_id = payload.command, payload.address_id
+
+        receiver = self._get_receiver(command)
+        responses = list(command.invoke(receiver))
+        if responses:
+            destination = command.response_destination
+            recipient_id = destination.get_recipient_id(address_id)
+            for response in responses:
+                yield Payload(response, recipient_id)
+
+    def _get_receiver(self, command: Command) -> Optional[Any]:
+        for registered_type, receiver in self._registered_types.items():
+            if isinstance(command, registered_type):
+                return receiver
+        raise CommandNotRegisteredException(command)
+
+
+class NoneReceiverException(Exception):
+    pass
+
+
+class CommandNotRegisteredException(Exception):
+    pass
