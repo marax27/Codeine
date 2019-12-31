6b9a77afe4002e9b8894ccb13c2e27f6ff575e9c
Kacper Tonia <kacpert314@gmail.com>
2019-12-31 19:20:31 +0100

    Create RegisterCommand and send it where needed

--- app/computing/domain_commands.py
+++ app/computing/domain_commands.py
@@ -35,3 +35,24 @@ def create_result_command(identifier_type: type, result_type: type) -> type:
         bases=(BaseResultCommand,),
         frozen=True
     )
+
+
+@dataclass(frozen=True)
+class BaseRegisterCommand(DomainCommand):
+    identifier: base.SubproblemId
+
+    @classmethod
+    def get_identifier(cls) -> str:
+        return 'REGISTER'
+
+    def invoke(self, receiver: base.SubproblemPool) -> List[Command]:
+        return []
+
+
+def create_register_command(identifier_type: type) -> type:
+    return make_dataclass(
+        'RegisterCommand',
+        (('identifier', identifier_type),),
+        bases=(BaseRegisterCommand,),
+        frozen=True
+    )
